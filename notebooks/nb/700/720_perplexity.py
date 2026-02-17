f0a16aff9611cc0e9448573df76ff281c836eb5e
Jani Sourander <jani.sourander@kamk.fi>
2026-02-17 14:40:31 +0200

    GPU memory usage tips added

--- notebooks/nb/700/720_perplexity.py
+++ notebooks/nb/700/720_perplexity.py
@@ -31,8 +31,9 @@ def _(mo):
 def _():
     import torch
     from transformers import AutoModelForCausalLM, AutoTokenizer
+    from functools import partial
 
-    return AutoModelForCausalLM, AutoTokenizer, torch
+    return AutoModelForCausalLM, AutoTokenizer, partial, torch
 
 
 @app.cell(hide_code=True)
@@ -63,7 +64,12 @@ def _(mo):
     mo.md(r"""
     ## Choose your device
 
-    Note that running the model would require a fair amount of memory if it would have to **fully fit** into VRAM or RAM. However, the safetensor format supports lazy-loading and partial data loading. It will run with less VRAM/RAM, but it will be slow.
+    Note that running the model requires a fair amount of memory – assuming it has to **fully fit** into your memory. However, the safetensor format allows lazy-loading and partial data loading, meaning that the model is sequantially loaded into memory layer by layer.... **when utilizing CPU**. It may be that CUDA can be made to work with this too, but without any code modification, this causes the following rule:
+
+    * if `< 16 GB` memory on your graphics card: `USE_GPU = False`
+    * if `>= 16 GB` memory on your graphics card: `USE_GPU = True`
+
+    Nevertheless, a curious mind tries both settings. Your GPU won't break. It will just cry a bit.
     """)
     return
 
@@ -89,26 +95,43 @@ def _(torch):
 def _(mo):
     mo.md(r"""
     ## Helper functions
+
+    We are using **partial function application** here from the functional programming paradigm.
     """)
     return
 
 
 @app.cell
-def _(device, model, tokenizer, torch):
-    def Perplexity(wiki_quote):
-        # 1. Tokenize (creates CPU tensors by default)
-        wiki_text = tokenizer(wiki_quote, return_tensors="pt")
+def _(device, model, partial, tokenizer, torch):
+    def _calculate_perplexity(model, tokenizer, device, wiki_quote):
+        model.eval()
+        with torch.no_grad():
+            # 1. Tokenize (creates CPU tensors by default)
+            wiki_text = tokenizer(wiki_quote, return_tensors="pt")
 
-        # 2. Move input tensors to the GPU (MPS)
-        input_ids = wiki_text["input_ids"].to(device)
+            # 2. Move input tensors to the GPU (MPS)
+            input_ids = wiki_text["input_ids"].to(device)
 
-        # 3. accurate calculation
-        loss = model(input_ids = input_ids,
-                     labels = input_ids).loss
+            # 3. accurate calculation
+            loss = model(input_ids = input_ids,
+                         labels = input_ids).loss
 
         return torch.exp(loss)
 
+    # Create the wrapper with dependencies pre-filled 
+    # to make next cells less verbose
+    Perplexity = partial(_calculate_perplexity, model, tokenizer, device)
     return (Perplexity,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    ## Playground
+
+    Find out how perplexed the model is with various input. Copy-pasting from Finnish wikipedia should keep the scores low (close to 1). Modifying those copy-pasted quotes should increase it. Writing utter nonsense should increase it even higher.
+    """)
+    return
 
 
 @app.cell
@@ -119,7 +142,7 @@ def _(Perplexity):
 
 @app.cell
 def _(Perplexity):
-    Perplexity("Ainola on puisto Oulussa, jossa taskulämmin sinikylkinen maistuu maukkaimmalta.")
+    Perplexity("Ainola on puisto Oulusa, jossa taskulämmin sinikylkinen maistuu maukkaimmalta.")
     return
 
 
